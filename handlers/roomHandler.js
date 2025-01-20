a913501f000d199bcfc47704fee3a67b98b35fa8
swapnil <swapnil130501@gmail.com>
2025-01-20 20:29:40 +0530

    Add rooms record to store users

--- handlers/roomHandler.js
+++ handlers/roomHandler.js
@@ -1,15 +1,21 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const uuid_1 = require("uuid");
+const rooms = {};
 const roomHandler = (socket) => {
     const createRoom = () => {
         const roomId = (0, uuid_1.v4)(); // create an unique room Id
         socket.join(roomId); // create a socket connection for the room
+        rooms[roomId] = [];
         socket.emit("room-created", { roomId }); // emit an event from server side
         console.log("Room created with id:", roomId);
     };
-    const joinedRoom = ({ roomId }) => {
-        console.log("New user has joined the room", roomId);
+    const joinedRoom = ({ roomId, peerId }) => {
+        if (rooms[roomId]) {
+            console.log("New user has joined the room", roomId, "with peer id as", peerId);
+            rooms[roomId].push(peerId);
+            socket.join(roomId);
+        }
     };
     socket.on('create-room', createRoom);
     socket.on('joined-room', joinedRoom);
--- src/handlers/roomHandler.ts
+++ src/handlers/roomHandler.ts
@@ -1,17 +1,27 @@
 import { Socket } from "socket.io";
 import { v4 as UUIDv4 } from "uuid";
+import IRoomParams from "../interfaces/IRoomParams";
+
+const rooms: Record<string, string[]> = {};
 
 const roomHandler = (socket: Socket) => {
 
     const createRoom = () => {
         const roomId = UUIDv4(); // create an unique room Id
         socket.join(roomId); // create a socket connection for the room
+
+        rooms[roomId] = [];
+
         socket.emit("room-created", { roomId }); // emit an event from server side
         console.log("Room created with id:", roomId);
     };
 
-    const joinedRoom = ({ roomId }: { roomId: string}) => {
-        console.log("New user has joined the room", roomId);
+    const joinedRoom = ({ roomId, peerId }: IRoomParams) => {
+        if(rooms[roomId]) {
+            console.log("New user has joined the room", roomId, "with peer id as", peerId);
+            rooms[roomId].push(peerId);
+            socket.join(roomId);
+        }
     };
 
     socket.on('create-room', createRoom);
