059532eacd4d028f67ce0f5d2f28d8dfdfeb93d0
swapnil <swapnil130501@gmail.com>
2025-01-21 10:20:40 +0530

    Add event to emit user join room and share media

--- handlers/roomHandler.js
+++ handlers/roomHandler.js
@@ -1,23 +1,56 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const uuid_1 = require("uuid");
+// the below map stores for a room what all peers have joined
+/**
+ * {1: [u1, u2, u3], 2: [u4,u5,u6]}
+ */
 const rooms = {};
 const roomHandler = (socket) => {
     const createRoom = () => {
-        const roomId = (0, uuid_1.v4)(); // create an unique room Id
-        socket.join(roomId); // create a socket connection for the room
-        rooms[roomId] = [];
-        socket.emit("room-created", { roomId }); // emit an event from server side
-        console.log("Room created with id:", roomId);
+        // this will be our unique room id in which multiple
+        // connection will exchange data
+        const roomId = (0, uuid_1.v4)();
+        // we will make the socket connection enter a new room
+        socket.join(roomId);
+        rooms[roomId] = []; // create a new entry for the room
+        // we will emit an event from server side that
+        // socket connection has been added to a room
+        socket.emit("room-created", { roomId });
+        console.log("Room created with id", roomId);
     };
+    /**
+     *
+     * The below function is executed everytime a user(creator or joinee) joins
+     * a new room
+     */
     const joinedRoom = ({ roomId, peerId }) => {
+        console.log("joined room called", rooms, roomId, peerId);
         if (rooms[roomId]) {
-            console.log("New user has joined the room", roomId, "with peer id as", peerId);
+            // If the given roomId exist in the in memory db
+            console.log("New user has joined room", roomId, "with peer id as", peerId);
+            // the moment new user joins, add the peerId to the key of roomId
             rooms[roomId].push(peerId);
-            socket.join(roomId);
+            console.log("added peer to room", rooms);
+            socket.join(roomId); // make the user join the socket room
+            // whenever anyone joins the room
+            socket.on("ready", () => {
+                // from the frontend once someone joins the room we will emit a ready event
+                // then from our server we will emit an event to all the clients connected
+                // that a new peer has added
+                socket.to(roomId).emit("user-joined", { peerId });
+            });
+            // below event is for logging purpose
+            socket.emit("get-users", {
+                roomId,
+                participants: rooms[roomId]
+            });
         }
     };
-    socket.on('create-room', createRoom);
-    socket.on('joined-room', joinedRoom);
+    // When to call the above two function ?
+    // We will call the above two function when the client will
+    // emit events top create room and join room
+    socket.on("create-room", createRoom);
+    socket.on("joined-room", joinedRoom);
 };
 exports.default = roomHandler;
--- src/handlers/roomHandler.ts
+++ src/handlers/roomHandler.ts
@@ -2,30 +2,71 @@ import { Socket } from "socket.io";
 import { v4 as UUIDv4 } from "uuid";
 import IRoomParams from "../interfaces/IRoomParams";
 
-const rooms: Record<string, string[]> = {};
+// the below map stores for a room what all peers have joined
+/**
+ * {1: [u1, u2, u3], 2: [u4,u5,u6]}
+ */
+
+const rooms : Record<string, string[]> = {};
 
 const roomHandler = (socket: Socket) => {
 
     const createRoom = () => {
-        const roomId = UUIDv4(); // create an unique room Id
-        socket.join(roomId); // create a socket connection for the room
+        // this will be our unique room id in which multiple
+        // connection will exchange data
+        const roomId = UUIDv4();
 
-        rooms[roomId] = [];
+        // we will make the socket connection enter a new room
+        socket.join(roomId);
 
-        socket.emit("room-created", { roomId }); // emit an event from server side
-        console.log("Room created with id:", roomId);
+        rooms[roomId] = []; // create a new entry for the room
+
+        // we will emit an event from server side that
+        // socket connection has been added to a room
+        socket.emit("room-created", { roomId });
+        console.log("Room created with id", roomId);
     };
 
-    const joinedRoom = ({ roomId, peerId }: IRoomParams) => {
+    /**
+     *
+     * The below function is executed everytime a user(creator or joinee) joins
+     * a new room
+     */
+    const joinedRoom = ({ roomId, peerId } : IRoomParams) => {
+        console.log("joined room called", rooms, roomId, peerId);
         if(rooms[roomId]) {
-            console.log("New user has joined the room", roomId, "with peer id as", peerId);
+            // If the given roomId exist in the in memory db
+            console.log("New user has joined room", roomId, "with peer id as", peerId);
+            // the moment new user joins, add the peerId to the key of roomId
             rooms[roomId].push(peerId);
-            socket.join(roomId);
+            console.log("added peer to room", rooms);
+            socket.join(roomId); // make the user join the socket room
+
+            // whenever anyone joins the room
+
+            socket.on("ready", () => {
+                // from the frontend once someone joins the room we will emit a ready event
+                // then from our server we will emit an event to all the clients connected
+                // that a new peer has added
+                socket.to(roomId).emit("user-joined", {peerId});
+            });
+
+            // below event is for logging purpose
+            socket.emit("get-users", {
+                roomId,
+                participants: rooms[roomId]
+            });
         }
     };
 
-    socket.on('create-room', createRoom);
-    socket.on('joined-room', joinedRoom);
+
+    // When to call the above two function ?
+
+
+    // We will call the above two function when the client will
+    // emit events top create room and join room
+    socket.on("create-room", createRoom);
+    socket.on("joined-room", joinedRoom);
 };
 
 export default roomHandler;
